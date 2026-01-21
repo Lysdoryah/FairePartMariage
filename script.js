378c61b933276332d143d0ee43ff0c6adeeb6f16
Lysdoryah <114756789+Lysdoryah@users.noreply.github.com>
2026-01-21 08:34:58 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Animation simple à l’ouverture
+document.addEventListener("DOMContentLoaded", () => {
+    const card = document.querySelector(".card");
+    card.style.opacity = 0;
+    card.style.transform = "translateY(20px)";
+  
+    setTimeout(() => {
+      card.style.transition = "all 0.8s ease";
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }, 100);
+  });
+  
