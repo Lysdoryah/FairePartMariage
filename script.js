864066e32ce3fba53b7d59c46612fb914d84038c
Lysdoryah <114756789+Lysdoryah@users.noreply.github.com>
2026-01-21 10:50:59 +0100

    vrai animation intégrée

--- script.js
+++ script.js
@@ -3,38 +3,12 @@ const envelopeScreen = document.getElementById("envelopeScreen");
 const invitationScreen = document.getElementById("invitationScreen");
 
 envelope.addEventListener("click", () => {
-  envelope.classList.add("open"); // lève le rabat
+  envelope.classList.remove("close");
+  envelope.classList.add("open");
 
   setTimeout(() => {
-    envelopeScreen.classList.add("hidden"); // masque l'enveloppe
-    invitationScreen.classList.remove("hidden"); // montre le faire-part
-    document.body.style.overflow = "auto"; // scroll activé
-  }, 800);
-});
-
-$( document ).ready(function() {
-    
-    var envelope = $('#envelope');
-    var btn_open = $("#open");
-    var btn_reset = $("#reset");
-    
-    envelope.click( function() {
-        open();
-    });
-    btn_open.click( function() {
-        open();
-    });
-    btn_reset.click( function() {
-        close();
-    });
-
-    function open() {
-        envelope.addClass("open")
-           .removeClass("close");
-    }
-    function close() {
-        envelope.addClass("close")
-           .removeClass("open");
-    }
-   
+    envelopeScreen.classList.add("hidden");
+    invitationScreen.classList.remove("hidden");
+    document.body.style.overflow = "auto";
+  }, 1200); // un peu plus de temps pour animation
 });
